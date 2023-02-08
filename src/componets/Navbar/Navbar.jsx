33651d6bea518f953fe51254d8223990d4288587
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-02-08 11:51:24 -0300

    cambios en el className

---
+++ src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+const Navbar = () => {
+
+    return (
+        <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+            <div className='container-fluid'>
+                <a className='navbar-brand' href="#">E-Commerce</a>
+                <button className='navbar-toggler' type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id="navbarNavAltMarkup">
+                    <div className='navbar-nav'>
+                        <a className='nav-link active' aria-current="page" href="#">Inicio</a>
+                        <a className='nav-link' href="#">Promociones</a>
+                        <a className='nav-link' href="#">Tienda</a>
+                        <div className='cartwidget'><CartWidget /></div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
